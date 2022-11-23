fdfb877680904216cf4a730281204b1dfbedde07
SepCode <chengfei01@qiyi.com>
2022-11-23 16:10:29 +0800

    原型模式

--- Design-Patterns-CN.playground/Pages/Creational.xcplaygroundpage/Contents.swift
+++ Design-Patterns-CN.playground/Pages/Creational.xcplaygroundpage/Contents.swift
@@ -250,12 +250,13 @@ class MoonWorker {
     let name: String
     var health: Int = 100
 
-    init(name: String) {
+    init(name: String, health: Int = 100) {
         self.name = name
+        self.health = health
     }
 
     func clone() -> MoonWorker {
-        return MoonWorker(name: name)
+        return MoonWorker(name: name, health: health)
     }
 }
 
@@ -264,12 +265,23 @@ class EarthWorker: NSObject, NSCopying {
     let name: String
     var health: Int = 100
 
-    init(name: String) {
+    required init(name: String, health: Int = 100) {
         self.name = name
+        self.health = health
     }
     
     func copy(with zone: NSZone? = nil) -> Any {
-        return EarthWorker(name: name)
+        return EarthWorker(name: name, health: health)
+    }
+}
+
+class ChinaWorker: EarthWorker {
+    var height: Int = 170
+    
+    override func copy(with zone: NSZone? = nil) -> Any {
+        let clone = super.copy() as! ChinaWorker
+        clone.height = height
+        return clone
     }
 }
 /*:
@@ -286,13 +298,15 @@ bell2.health = 23
 var bell3 = prototype.clone()
 bell3.health = 0
 
-let person = EarthWorker(name: "Man")
+let person = ChinaWorker(name: "Man")
 
-var man1 = person.copy() as! EarthWorker
+var man1 = person.copy() as! ChinaWorker
 man1.health = 12
+/*:
+ ### 理解:
+ 原型模式多用于创建复杂的或者耗时的实例，因为这种情况下，复制一个已经存在的实例使程序运行更高效；或者创建值相等，只是命名不一样的同类数据。
+ */
 
-//: ### 理解:
-//: 原型模式多用于创建复杂的或者耗时的实例，因为这种情况下，复制一个已经存在的实例使程序运行更高效；或者创建值相等，只是命名不一样的同类数据。
 /*:
 💍 单例（Singleton）
 --------------
@@ -315,6 +329,9 @@ final class ElonMusk {
 ### 用法
 */
 let elon = ElonMusk.shared // There is only one Elon Musk folks.
-//: ### 理解:
-//:实现单例模式的思路是：一个类能返回对象一个引用（永远是同一个）和一个获得该实例的方法（必须是静态方法，通常使用getInstance这个名称）；当我们调用这个方法时，如果类持有的引用不为空就返回这个引用，如果类保持的引用为空就创建该类的实例并将实例的引用赋予该类保持的引用；同时我们还将该类的构造函数定义为私有方法，这样其他处的代码就无法通过调用该类的构造函数来实例化该类的对象，只有通过该类提供的静态方法来得到该类的唯一实例。
-//:单例模式在多线程的应用场合下必须小心使用。如果当唯一实例尚未创建时，有两个线程同时调用创建方法，那么它们同时没有检测到唯一实例的存在，从而同时各自创建了一个实例，这样就有两个实例被构造出来，从而违反了单例模式中实例唯一的原则。 解决这个问题的办法是为指示类是否已经实例化的变量提供一个互斥锁（虽然这样会降低效率）。
+/*:
+ ### 理解:
+ 实现单例模式的思路是：一个类能返回对象一个引用（永远是同一个）和一个获得该实例的方法（必须是静态方法，通常使用getInstance这个名称）；当我们调用这个方法时，如果类持有的引用不为空就返回这个引用，如果类保持的引用为空就创建该类的实例并将实例的引用赋予该类保持的引用；同时我们还将该类的构造函数定义为私有方法，这样其他处的代码就无法通过调用该类的构造函数来实例化该类的对象，只有通过该类提供的静态方法来得到该类的唯一实例。
+ 
+ 单例模式在多线程的应用场合下必须小心使用。如果当唯一实例尚未创建时，有两个线程同时调用创建方法，那么它们同时没有检测到唯一实例的存在，从而同时各自创建了一个实例，这样就有两个实例被构造出来，从而违反了单例模式中实例唯一的原则。 解决这个问题的办法是为指示类是否已经实例化的变量提供一个互斥锁（虽然这样会降低效率）。
+ */
